3dcd65479025eb69a64d59d45026ffea4d0e44fa
unknown <nishantranjj91555@gmail.com>
2023-07-31 19:17:24 +0530

    fibonacci

---
+++ commaOp.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    int i=0;
+    if(i=15,10,5)
+    printf("Programming %d",i);
+    else
+    printf("Skill %d",i);
+}
---
+++ fibonacci.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("\nSKY");
+    printf("G\bAYA");
+}
